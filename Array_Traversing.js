25101bb776dda1e4143aadc0614e4f9ec1d352ed
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-01-25 17:51:01 +0530

    Create Array_Traversing.js

---
+++ Array_Traversing.js
@@ -0,0 +1,12 @@
+let data = [5,8,67,54,34,65,98,56,2,53,23];
+for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    console.log(`Array is ${i} is ${data[i]} `);
+
+
+    let x = 5;
+    // document.write(data[x]);
+    console.log(data[x]);
+    
+}
+// console.log(element);
