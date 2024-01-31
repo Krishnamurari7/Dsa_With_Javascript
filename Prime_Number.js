c5ad3a458ce9625f0e8a210a135da9f8317c8b6d
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-01-31 22:34:06 +0530

    Update Prime_Number.js

--- Prime_Number.js
+++ Prime_Number.js
@@ -35,3 +35,24 @@ if(number == 1){
     }
     console.log(res);
 }
+
+
+//third method
+function isPrime(number) {
+    if (number <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  const numToCheck = 17;
+  if (isPrime(numToCheck)) {
+    console.log(`${numToCheck} is a prime number.`);
+  } else {
+    console.log(`${numToCheck} is not a prime number.`);
+  }
+  
