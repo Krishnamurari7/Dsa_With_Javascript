8bee5b336ed54731e8c05c8840ce34713352aba6
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-02-02 15:56:32 +0530

    Create reversestring.js

---
+++ reversestring.js
@@ -0,0 +1,9 @@
+let str = 'sonu is intelligent';
+const reverseString = str.split(' ')
+const reverse = str.split('')
+const count = reverseString.length;
+const rever = reverse.length - count;
+console.log(`total letter are == ${rever}`);
+console.log(`total space are == ${count}`);
+const rev = reverseString.reverse().join(''); 
+console.log(`reverse string are == ${rev}`);
