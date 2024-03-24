cb9be52b44f000d53fdabfab3b06ccdfe7914490
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-03-24 23:39:53 +0530

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,14 +1,18 @@
 
-function generateOTP() { 
-	let digits = '0123456789'; 
-	let OTP = ''; 
-	let len = digits.length 
-	for (let i = 0; i < 4; i++) { 
-		OTP += digits[Math.floor(Math.random() * len)]; 
-	} 
+// function generateOTP() { 
+// 	let digits = '0123456789'; 
+// 	let OTP = ''; 
+// 	let len = digits.length 
+// 	for (let i = 0; i < 4; i++) { 
+// 		OTP += digits[Math.floor(Math.random() * len)]; 
+// 	} 
 	
-	return OTP; 
-} 
+// 	return OTP; 
+// } 
 
-console.log("OTP of 4 digits: ") 
-console.log(generateOTP());
+// console.log("OTP of 4 digits: ") 
+// console.log(generateOTP());
+
+import otpGenerator from otpGenerator
+
+otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
